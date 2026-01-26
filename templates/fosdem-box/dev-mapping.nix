82395b61065de45554635eb8d343e318b1164164
dexterlb <dexterlb@qtrp.org>
2026-01-26 20:35:35 +0200

    correct addresses for habibis

--- templates/fosdem-box/dev-mapping.nix
+++ templates/fosdem-box/dev-mapping.nix
@@ -3,8 +3,8 @@
 
   mixos.devMap = {
     videoCapture.by-path = {
-      "pci-0000:05:00.0-usbv3-0:1:1.0:fixme1" = { name = "video-slides"; };
-      "pci-0000:05:00.0-usbv3-0:1:1.0:fixme2" = { name = "video-cam"; };
+      "pci-0000:00:14.0-usbv3-0:1:1.0" = { name = "video-slides"; };
+      "pci-0000:00:14.0-usbv3-0:2:1.0" = { name = "video-cam"; };
     };
 
     audio.by-name = {
